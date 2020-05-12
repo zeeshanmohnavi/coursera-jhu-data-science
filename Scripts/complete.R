593952f34195d1aa2ca7c9cd7a7dfe5b263882e1
zeeshanmohnavi <22724928+zeeshanmohnavi@users.noreply.github.com>
2020-05-12 21:37:00 +0530

    Add sample test cases

--- Scripts/complete.R
+++ Scripts/complete.R
@@ -35,29 +35,53 @@ complete <- function(directory, id = 1:332) {
   df_complete
 }
 
-## Test code block for complete()
-
-complete("./Data/specdata/", 1)
-##   id nobs
-## 1  1  117
-
-complete("./Data/specdata/", c(2, 4, 8, 10, 12))
-##   id nobs
-## 1  2 1041
-## 2  4  474
-## 3  8  192
-## 4 10  148
-## 5 12   96
-
-complete("./Data/specdata/", 30:25)
-##   id nobs
-## 1 30  932
-## 2 29  711
-## 3 28  475
-## 4 27  338
-## 5 26  586
-## 6 25  463
-
-complete("./Data/specdata/", 3)
-##   id nobs
-## 1  3  243
+# ## Test code block for complete()
+# 
+# # Test case 1
+# complete("./Data/specdata/", 1)
+# ##   id nobs
+# ## 1  1  117
+# 
+# # Test case 2
+# complete("./Data/specdata/", c(2, 4, 8, 10, 12))
+# ##   id nobs
+# ## 1  2 1041
+# ## 2  4  474
+# ## 3  8  192
+# ## 4 10  148
+# ## 5 12   96
+# 
+# # Test case 3
+# complete("./Data/specdata/", 30:25)
+# ##   id nobs
+# ## 1 30  932
+# ## 2 29  711
+# ## 3 28  475
+# ## 4 27  338
+# ## 5 26  586
+# ## 6 25  463
+# 
+# # Test case 4
+# complete("./Data/specdata/", 3)
+# ##   id nobs
+# ## 1  3  243
+# 
+# ## Quiz test cases
+# 
+# # Test case 5
+# cc <- complete("./Data/specdata/", c(6, 10, 20, 34, 100, 200, 310))
+# print(cc$nobs)
+# # [1] 228 148 124 165 104 460 232
+# 
+# # Test case 6
+# cc <- complete("./Data/specdata/", 54)
+# print(cc$nobs)
+# # [1] 219
+# 
+# # Test case 7
+# RNGversion("3.5.1")  
+# set.seed(42)
+# cc <- complete("./Data/specdata/", 332:1)
+# use <- sample(332, 10)
+# print(cc[use, "nobs"])
+# # [1] 711 135  74 445 178  73  49   0 687 237
