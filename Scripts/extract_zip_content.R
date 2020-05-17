7738b4bd5b04eb535953c68ffbe017958e61ff76
zeeshanmohnavi <22724928+zeeshanmohnavi@users.noreply.github.com>
2020-05-17 13:47:09 +0530

    Define the name of the zip file in a separate variable to enhance code maintainability

--- Scripts/extract_zip_content.R
+++ Scripts/extract_zip_content.R
@@ -1,3 +1,6 @@
+file_name <- "ProgAssignment3-data"
+
 target_dir <- paste(getwd(), "/Data/", sep = "")
-source_zip_file <- paste(getwd(), "/Data/specdata.zip", sep = "")
+source_zip_file <- paste(getwd(), paste("/Data/", file_name, ".zip", sep = ""), sep = "")
+
 unzip(source_zip_file, exdir = target_dir)
