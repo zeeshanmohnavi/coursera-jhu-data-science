546122103e8000ca43b21a4563c15441cfe380ab
zeeshanmohnavi <22724928+zeeshanmohnavi@users.noreply.github.com>
2020-06-06 00:01:31 +0530

    Add the option to specify the folder/directory name to unzip the contents into

--- Scripts/extract_zip_content.R
+++ Scripts/extract_zip_content.R
@@ -1,6 +1,11 @@
-file_name <- "ProgAssignment3-data"
+file_name <- "UCI HAR Dataset"
+dir_name <- "course3-project"
+target_dir <- paste(getwd(), "/Data/", dir_name, sep = "")
 
-target_dir <- paste(getwd(), "/Data/", sep = "")
+if (!file.exists(target_dir)) {
+  dir.create(target_dir)
+}
+
 source_zip_file <- paste(getwd(), paste("/Data/", file_name, ".zip", sep = ""), sep = "")
 
 unzip(source_zip_file, exdir = target_dir)
